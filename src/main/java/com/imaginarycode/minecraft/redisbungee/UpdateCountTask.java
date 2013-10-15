92fc64b7132117fa7405b4f1ec2473298124d665
Tux <write@imaginarycode.com>
2013-10-14 23:42:23 -0400

    Add WTFPL header

--- src/main/java/com/imaginarycode/minecraft/redisbungee/UpdateCountTask.java
+++ src/main/java/com/imaginarycode/minecraft/redisbungee/UpdateCountTask.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright © 2013 tuxed <write@imaginarycode.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
 package com.imaginarycode.minecraft.redisbungee;
 
 import redis.clients.jedis.Jedis;
