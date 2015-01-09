6511e2154faae955c690016f4e544ac761f5a786
vemacs <gentoo@outlook.com>
2015-01-09 07:44:03 -0700

    Revert "Revert to old method as the new method has changed"
    This reverts commit 16ce4eb6965ac3166819f1d9e70494beb30d72a5.

--- src/main/java/com/imaginarycode/minecraft/redisbungee/util/NameFetcher.java
+++ src/main/java/com/imaginarycode/minecraft/redisbungee/util/NameFetcher.java
@@ -6,46 +6,21 @@
  */
 package com.imaginarycode.minecraft.redisbungee.util;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteStreams;
 import com.google.gson.reflect.TypeToken;
 import com.imaginarycode.minecraft.redisbungee.RedisBungee;
 
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.Callable;
+import java.net.URLConnection;
+import java.util.*;
 
-/* Credits to evilmidget38 for this class. I modified it to use Gson. */
-class NameFetcher implements Callable<Map<UUID, String>> {
-    private static final String PROFILE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
-    private final List<UUID> uuids;
-
-    public NameFetcher(List<UUID> uuids) {
-        this.uuids = ImmutableList.copyOf(uuids);
-    }
-
-    @Override
-    public Map<UUID, String> call() throws Exception {
-        Map<UUID, String> uuidStringMap = new HashMap<>();
-        for (UUID uuid : uuids) {
-            HttpURLConnection connection = (HttpURLConnection) new URL(PROFILE_URL + uuid.toString().replace("-", "")).openConnection();
-            Map<String, String> response = RedisBungee.getGson().fromJson(new InputStreamReader(connection.getInputStream()), new TypeToken<Map<String, String>>() {
-            }.getType());
-            String name = response.get("name");
-            if (name == null) {
-                continue;
-            }
-            String cause = response.get("cause");
-            String errorMessage = response.get("errorMessage");
-            if (cause != null && cause.length() > 0) {
-                throw new IllegalStateException(errorMessage);
-            }
-            uuidStringMap.put(uuid, name);
-        }
-        return uuidStringMap;
+public class NameFetcher {
+    public static List<String> nameHistoryFromUuid(UUID uuid) throws IOException {
+        URLConnection connection = new URL("https://api.mojang.com/user/profiles/" + uuid.toString().replace("-", "").toLowerCase() + "/names").openConnection();
+        String text = new String(ByteStreams.toByteArray(connection.getInputStream()));
+        Type listType = new TypeToken<List<String>>() {}.getType();
+        return RedisBungee.getGson().fromJson(text, listType);
     }
 }
