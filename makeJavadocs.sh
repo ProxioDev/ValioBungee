87aaf22073ecf34ece3d156b05433a80861990a4
mohammed jasem alaajel <xrambad@gmail.com>
2022-07-16 02:22:46 +0400

    add  to makeJavaDocs

--- makeJavadocs.sh
+++ makeJavadocs.sh
@@ -1,3 +1,3 @@
 rm -rf javadoc
 mkdir javadoc
-mvn javadoc:javadoc -pl RedisBungee-API,RedisBungee-Bungee,RedisBungee-Velocity -am
+mvn clean install javadoc:javadoc -pl RedisBungee-API,RedisBungee-Bungee,RedisBungee-Velocity -am
