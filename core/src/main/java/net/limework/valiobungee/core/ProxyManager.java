f1b744bfbe515efb10a744013758437d02adbae6
Mohammed Alteneiji <xrambad@glomc.net>
2026-04-01 10:32:49 +0400

    fix license notices

--- core/src/main/java/net/limework/valiobungee/core/ProxyManager.java
+++ core/src/main/java/net/limework/valiobungee/core/ProxyManager.java
@@ -1,10 +1,21 @@
 /*
-* Copyright (c) 2026-present ValioBungee contributors
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU GENERAL PUBLIC LICENSE Version 3
-* which accompanies this distribution, and is available at
-* https://www.gnu.org/licenses/gpl-3.0.txt
-*/
+ * Copyright (c) 2026 ValioBungee contributors
+ *
+ * This file is part of ValioBungee.
+ *
+ * ValioBungee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ValioBungee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ValioBungee. If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
+ */
 package net.limework.valiobungee.core;
 
 import com.github.benmanes.caffeine.cache.Cache;
