9cb81330e9b97c01919d7dfb5a60e79a1499b627
Mohammed Alteneiji <xrambad@glomc.net>
2026-04-01 12:56:30 +0400

    make an api version

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,21 @@
 description = "Api functions for valiobungee"
 
+plugins {
+    alias(libs.plugins.blossom)
+    alias(libs.plugins.indragit)
+}
+
+
+sourceSets {
+    main {
+        blossom {
+            javaSources {
+                property("apiversion", project.property("api-version").toString())
+            }
+        }
+    }
+}
+
 java {
     withJavadocJar()
     withSourcesJar()
